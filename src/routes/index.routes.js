8c8e17c44a76cf0edab0422cb0bfc149abf7f1e6
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-11-05 02:05:12 -0400

    agregando los controladores y ordenando mucho mas el codigo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import { pool } from "../db.js";
+import { pong } from '../controllers/index.controller.js';
+
 
 const router = Router();
 
-router.get('/ping', async (req, res) => { // con esto nos daremos cuenta si estamos conectados a la bd
-    const result = await pool.query('SELECT 1+1 AS result');
-    res.json(result);
-});
+router.get('/ping', pong);
 
 
 export default router;
