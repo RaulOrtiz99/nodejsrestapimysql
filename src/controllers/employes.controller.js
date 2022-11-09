cdd3cf7a92e9b1745c6ed154284e027ac9423a71
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-11-08 20:59:02 -0400

    agregando el metodo para buscar con un id en especifico ademas de agregar un 404 en caso de no encontrarlo

--- src/controllers/employes.controller.js
+++ src/controllers/employes.controller.js
@@ -2,23 +2,37 @@ import { pool } from "../db.js";
 
 //estos son los controladores
 
+export const getEmployees = async (req, res) => {
+  // esto es para obtener todos los empleados
+  const [rows] = await pool.query("SELECT * FROM employee");
+  res.json(rows);
+};
 
-export const getEmployees = async (req, res) => {// esto es para obtener todos los empleados
-    const [rows] = await pool.query('SELECT * FROM employee');
-    res.json(rows);
+export const getEmployee = async (req, res) => {
+  console.log(req.params.id); //esto es para obtener un id en especifico
+  const [rows] = await pool.query("SELECT * FROM employee WHERE id =?", [
+    req.params.id,
+  ]);
+  //res.send(rows); esto es para enviar todo el json completo
+  if (rows.length <= 0)
+    return res.status(404).json({
+      // esto es para el manejo de errores es decir que pasa si no se encuentra el empleado por el id
+      message: "Employee not found",
+    });
+  res.json(rows[0]);
 };
 
 export const createEmployee = async (req, res) => {
-    const { name, salary } = req.body;
-    const [rows] = await pool.query(
-        "INSERT INTO employee (name,salary)VALUES(?,?)",
-        [name, salary]
-    );
-    res.send({
-        id: rows.insertId,
-        name,
-        salary,
-    });
+  const { name, salary } = req.body;
+  const [rows] = await pool.query(
+    "INSERT INTO employee (name,salary)VALUES(?,?)",
+    [name, salary]
+  );
+  res.send({
+    id: rows.insertId,
+    name,
+    salary,
+  });
 };
 export const updateEmployee = (req, res) => res.send("actualizando empleados");
 
--- src/routes/employes.routes.js
+++ src/routes/employes.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getEmployees, createEmployee, updateEmployee, deleteEmployee } from '../controllers/employes.controller.js'
+import { getEmployees, createEmployee, updateEmployee, deleteEmployee, getEmployee } from '../controllers/employes.controller.js'
 
 const router = Router();
 
 
 router.get('/employees', getEmployees); //asi se manejan las rutas y el tema de los controladores
+
+router.get('/employees/:id', getEmployee);
 
 router.post('/employees', createEmployee);
 
